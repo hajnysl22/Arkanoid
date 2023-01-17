ef73a8f068faae9909ad0354116561ccc0b4cf70
Lukáš Hajnyš <hajnysl22@spstrutnov.cz>
2023-01-17 18:06:18 +0100

    FPS

--- arkanoid.py
+++ arkanoid.py
@@ -10,9 +10,14 @@ screen_height = 1000
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Arkanoid")
 
+# FPS
+clock = pygame.time.Clock()
+
 # Game Loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    clock.tick(60)
