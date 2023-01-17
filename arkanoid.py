e957ab651d4ca629108038e4a38965105fd0d63b
Lukáš Hajnyš <hajnysl22@spstrutnov.cz>
2023-01-17 18:05:20 +0100

    Screen

--- arkanoid.py
+++ arkanoid.py
@@ -3,6 +3,13 @@ import pygame
 # Setup
 pygame.init()
 
+screen_width = 500
+screen_height = 1000
+
+# Game Window
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption("Arkanoid")
+
 # Game Loop
 while True:
     for event in pygame.event.get():
